ab3d5f9b94de74f84120e605de8c3d39dd86e29d
anuva08 <77385864+anuva08@users.noreply.github.com>
2021-04-04 09:47:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -69,7 +69,7 @@ function draw() {
  
 }
 function keyPressed() { 
-  if (keyCode === UP_ARROW) { 
+  if (keyCode === LEFT_ARROW) { 
   Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-100,y:-95}); 
 } 
 }
